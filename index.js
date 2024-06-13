22de0ae04ca9fbebfe5d2c1f9f5a0279e040a196
sivasathish889 <sivasathish889@gmail.com>
2024-06-13 23:01:26 +0530

    gsap added

--- index.js
+++ index.js
@@ -13,8 +13,45 @@ let btn=document.getElementById("dark-btn");
       
       })
 
-
 let date = new Date
 let year = document.getElementById("copyrights-year")
 year.innerHTML = date.getFullYear()
 
+
+
+gsap.from(".first-section h3", 1, {
+  x : -600
+})
+gsap.from(".first-section h1", 2, {
+  x : -600,
+  delay : .7
+})
+gsap.from(".first-section .h4", 2.4, {
+  x : -600,
+  delay : 1.6
+})
+gsap.from("#my-img1", 2, {
+  x : 600,
+  duration : 2
+})
+
+gsap.from("#navbarText ul li ", .6, {
+  y :-250,
+  stagger : .6
+})
+gsap.from("#linkedin", .4, {
+  scale : 0,
+  opacity : 0
+})
+gsap.from("#github", .8, {
+  scale : 0,
+  opacity : 0
+})
+gsap.from("#insta", 1.2, {
+  scale : 0,
+  opacity : 0
+})
+gsap.from("#facebook", 1.6, {
+  scale : 0,
+  opacity : 0
+})
